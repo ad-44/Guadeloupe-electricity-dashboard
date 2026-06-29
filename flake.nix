e4131ad4c8c41d4a0f1f73351e2f4b4e072192fc
ad-44 <antoine.debille@gmail.com>
2026-06-30 00:29:57 +0200

    Added pkgs (pandoc) in the environment to fix mismatch between pandoc and quarto version.

--- flake.nix
+++ flake.nix
@@ -18,16 +18,91 @@
         ps.altair
         ps.requests
         ps.itables
-        ps.jupyter
+        ps.jupyter-console
+        ps.tabulate
+        ps.pyyaml
+        ps.nbformat
+        ps.nbclient
         ]);
+        
+        pythonrc = pkgs.writeText "pythonrc.py" ''
+import pandas as pd
+import subprocess
+from tabulate import tabulate
+import tempfile
+
+def pt(df):
+  print(tabulate(df, headers="keys", tablefmt="psql"))
+
+def get_repl_pane():
+  out = subprocess.check_output(
+    [
+      "tmux", "list-panes", "-a", "-F", "#{pane_id} #{pane_title}"
+    ], text=True
+  )
+
+  for line in out.splitlines():
+    pane_id, title = line.split(" ",1)
+    if title == "REPL":
+      return pane_id
+
+  return None
+
+def get_right_pane(repl_pane):
+    out = subprocess.check_output(
+        [
+            "tmux", "list-panes", "-a", "-F", "#{pane_id} #{pane_left} #{pane_top}"
+        ], text=True
+    )
+
+    panes = []
+    for line in out.splitlines():
+        pid, left, top = line.split()
+        panes.append(pid)
+
+    try:
+        idx = panes.index(repl_pane)
+        return panes[idx + 1]
+    except:
+        return None 
+    
+def vd(df):
+    path = tempfile.mktemp(suffix='.csv')
+    df.to_csv(path, index=False)
+
+    repl = get_repl_pane()
+    if not repl:
+        raise RuntimeError("REPL pane does not exist")
+
+    vd_pane = get_right_pane(repl)
+
+    if not vd_pane:
+        subprocess.run(
+            [
+                "tmux", "split-window", "-h", "-t", repl, f"vd {path}"
+            ]
+        )
+        return
+
+    subprocess.run(
+        [
+            "tmux", "send-keys", "-t", vd_pane, f"o {path}", "Enter"
+        ]
+    )       
+    '';
+
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           python
           quarto
           black
+          pandoc
         ];
         
+      shellHook = ''
+        export PYTHONSTARTUP=${pythonrc}
+      '';
       };
     };
 }
